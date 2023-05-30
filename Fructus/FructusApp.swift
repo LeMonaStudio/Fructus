5a26352b314da17337d03d34493cd48230980b3c
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-05-30 16:11:13 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Musa Lawrence on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
